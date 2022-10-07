89eb3217c80b9b8cc90c7fe6350df7c3e57f334f
tranlehoangtu <tranlehoangtu2000@gmail.com>
2022-10-07 10:43:33 +0700

    update backend

---
+++ Backend/Project_Planning/src/test/java/com/javacode/Project_Planning/ProjectPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacode.Project_Planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
